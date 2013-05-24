6fc1de33c062f1e9050b4e981b5816420e48b5ef
riklomas <lomas.rik@gmail.com>
2013-05-24 13:51:49 +0100

    first commit

---
+++ app/models/order_product.rb
@@ -0,0 +1,6 @@
+class OrderProduct < ActiveRecord::Base
+  attr_accessible :order_id, :price_in_pence, :product_id, :quantity
+
+  belongs_to :order
+  belongs_to :product
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fruitly::Application.config.secret_token = '66166b8fdd4f9d5bb741344dd2f8ff802627f89dde100d37c36b97352b02bb7895157d26a848f221de92a4f9a0b8d070ac723410e8a3bd0738895dbf53d5d465'
