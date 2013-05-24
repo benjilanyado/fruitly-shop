b9b32f8905a9a4707c32b7cf800af453714c6e82
Benji Lanyado <benjilanyado@gmail.com>
2013-05-24 15:59:52 +0100

    stripe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+	def quid(num)
+		number_to_currency (num/100.0), unit: "&pound"
+	end
 end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -3,4 +3,6 @@ class OrderProduct < ActiveRecord::Base
 
   belongs_to :order
   belongs_to :product
+
+  delegate :title, :description, :image, to: :product
 end
